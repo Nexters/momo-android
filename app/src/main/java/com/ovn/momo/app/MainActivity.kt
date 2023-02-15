11becf4110e70af76ca255a119a56c6795e8a4c3
JGeun <68798525+JGeun@users.noreply.github.com>
2023-02-15 23:24:26 +0900

    Revert "feat #2: Navigation 구현 "

--- app/src/main/java/com/ovn/momo/app/MainActivity.kt
+++ app/src/main/java/com/ovn/momo/app/MainActivity.kt
@@ -3,23 +3,40 @@ package com.ovn.momo.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.ovn.momo.core.navigation.AppComposeNavigator
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.ovn.momo.feature.theme.MomoTheme
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-	@Inject
-	internal lateinit var appComposeNavigator: AppComposeNavigator
-
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
 			MomoTheme {
-				MomoMain(composeNavigator = appComposeNavigator)
+				// A surface container using the 'background' color from the theme
+				Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+					Greeting("Android")
+				}
 			}
 		}
+	}
+}
+
+@Composable
+fun Greeting(name: String) {
+	Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+	MomoTheme {
+		Greeting("Android")
 	}
 }
