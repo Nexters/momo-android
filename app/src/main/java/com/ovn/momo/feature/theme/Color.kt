e9c34d381dfbd660f710a72e64b36797497da1ad
YChaeeun <ychae.leah@gmail.com>
2023-02-19 22:28:35 +0900

    feat #9: 컬러코드 추가

--- app/src/main/java/com/ovn/momo/feature/theme/Color.kt
+++ app/src/main/java/com/ovn/momo/feature/theme/Color.kt
@@ -2,7 +2,20 @@ package com.ovn.momo.feature.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val MainColor = Color(0xFF946BEA)
+
+val Background = Color(0xFFF6F6F6)
+val TextBox1 = Color(0xFFF8F8F9)
+val TextBox2 = Color(0xFFF8F8FE)
+val PastBox = Color(0xFFF8F8FE)
+
+val Warning = Color(0xFFFF6464)
+
+val AttendanceChecked = Color(0xFF7FCBE5)
+val AttendanceLate = Color(0xFFFFC531)
+val NoticedAbsence = Color(0xFFFF8B32)
+
+val FontGray800 = Color(0xFF222222)
+val FontGray700 = Color(0xFF4F505C)
+val FontGray600 = Color(0xFF727484)
+val FontGray500 = Color(0xFFB3B6C5)
