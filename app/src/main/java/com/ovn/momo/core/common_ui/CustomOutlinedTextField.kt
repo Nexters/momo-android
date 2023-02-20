0029bbac9b021d0d4a13dea160b16f412cc98c3c
JGeun <pjk8926@gmail.com>
2023-02-20 15:40:32 +0900

    refactor #7: CustomOutlinedTextField Color 적용

--- app/src/main/java/com/ovn/momo/core/common_ui/CustomOutlinedTextField.kt
+++ app/src/main/java/com/ovn/momo/core/common_ui/CustomOutlinedTextField.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
@@ -18,25 +17,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.ovn.momo.R
-import com.ovn.momo.feature.theme.pretendard
+import com.ovn.momo.feature.theme.*
 
 @Composable
 @OptIn(ExperimentalComposeUiApi::class)
 fun CustomOutlinedTextField(
 	paddingDp: Int = 0,
 	text: String = "",
-	textFontFamily: FontFamily = pretendard,
 	placeHolder: String = "",
 	textVisualTransformation: VisualTransformation = VisualTransformation.None,
 	isError: State<Boolean> = remember { mutableStateOf(false) },
@@ -45,17 +36,11 @@ fun CustomOutlinedTextField(
 	) {
 
 	val keyboardController = LocalSoftwareKeyboardController.current
-
-	val textFieldUnFocusedColor = colorResource(id = R.color.textfield_unfocused_color)
-	val textFieldFocusedColor = colorResource(id = R.color.textfield_focused_color)
-	val borderUnFocusedColor = colorResource(id = R.color.textfield_border_unfocused_color)
-	val borderFocusedColor = colorResource(id = R.color.textfield_border_focused_color)
-	val errorColor = colorResource(id = R.color.textfield_border_error_color)
-
 	val focusRequester by remember { mutableStateOf(FocusRequester()) }
 
 	// TextField 안에 사용될 text
 	val textData = rememberSaveable { mutableStateOf(text) }
+
 	// 현재 컴포넌트가 focus되어있는지 체크하는 변수
 	var isFocused by rememberSaveable { mutableStateOf(false) }
 
@@ -72,7 +57,7 @@ fun CustomOutlinedTextField(
 					isActive = isFocused || textData.value.isNotEmpty()
 				}
 				.background(
-					color = if (isActive) textFieldFocusedColor else textFieldUnFocusedColor,
+					color = if (isActive) FocusedBox else UnFocusedBox,
 					shape = RoundedCornerShape(6.dp))
 				.fillMaxWidth(),
 			value = textData.value,
@@ -85,20 +70,15 @@ fun CustomOutlinedTextField(
 			},
 			placeholder = { Text(placeHolder) },
 			maxLines = 1,
-			textStyle = TextStyle(
-				fontSize = 16.sp,
-				color = Color.Black,
-				fontFamily = textFontFamily,
-				fontWeight = FontWeight.W500
-			),
+			textStyle = Typography.body1,
 			keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
 			keyboardActions = KeyboardActions {
 				keyboardController?.hide()
 			},
 			colors = TextFieldDefaults.outlinedTextFieldColors(
-				focusedBorderColor = borderFocusedColor,
-				unfocusedBorderColor = borderUnFocusedColor,
-				errorBorderColor = errorColor
+				focusedBorderColor = FocusedBorder,
+				unfocusedBorderColor = UnFocusedBorder,
+				errorBorderColor = Warning
 			),
 			isError = isError.value
 		)
@@ -106,8 +86,8 @@ fun CustomOutlinedTextField(
 		if (isError.value && errorMsg.value.isNotEmpty()) {
 			Text(
 				text = errorMsg.value,
-				color = errorColor,
-				style = MaterialTheme.typography.body1,
+				color = Warning,
+				style = Typography.body2,
 				modifier = Modifier.padding(start=3.dp, top = 2.dp)
 			)
 		}
